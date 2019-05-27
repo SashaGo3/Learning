823c34b309ea6e9b683fdd6e460725b957ee47f8
Alex <alexandr.gorbunov@techmagic.co>
2019-05-27 09:07:02 +0300

    New

---
+++ new_task.js
@@ -0,0 +1,35 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName("button"),
+    circle = document.getElementsByClassName("circle"),
+    heart = document.querySelectorAll(".heart");
+    console.log(btn);
+    console.log(btn[0]);
+    console.log(circle[2]);
+    console.log(heart);
+
+
+
+
+
+
+
+
+// // let arr = [1,12,2];
+// //     i = arr.sort(comparenum);
+
+// //     function comparenum(a,b){
+// //         return b-a;
+// //     }
+// //     console.log(arr);
+
+// let soldier = {
+//     health: 400,
+//     armor: 100
+// };
+// let john ={
+//     health:100
+// };
+
+// john.__proto__ = soldier;
+// console.log(john);
+// console.log(john.armor);
